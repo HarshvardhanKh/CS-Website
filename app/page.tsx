ca44b7a24f424ec1fa0ead158b60bc28e76a0529
Vidhyanshu Kumar <vidhyanshukumar@Vidhyanshus-MacBook-Air.local>
2026-02-14 01:32:19 +0530

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Footer from "@/src/components/common/Footer";
 import {Skiper19} from "@/src/components/ui/stroke"
 import ImageHover from "@/src/components/common/ImageHover";
+import Navbar from "@/src/components/common/Navbar";
+
 export default function Home() {
   return (
     <>
-
-    <Skiper19 />
+    <div><Navbar/></div>
+    <div><Skiper19 />
       <ImageHover/>
       <Footer/>
-
+</div>
+    
     
       
     </>
--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -11,7 +11,7 @@ export default function FooterWrapper() {
           "IEEE COMPUTER SOCIETY",
           "INNOVATION",
           "TECHNOLOGY",
-          "FUTURE"
+          "FUTURE",
           "IEEE CS"
         ]}
       />
