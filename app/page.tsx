79b42be6e5a00fde717a25c25fe5f638eb1fbb5a
HarshvardhanKh <harshvardhan.khaitan1202@gmail.com>
2026-02-15 00:04:53 +0530

    Added Horizontal Gallery Component + Modified next.config.txs to access images through http

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,18 @@ import Footer from "@/src/components/common/Footer";
 import {Skiper19} from "@/src/components/ui/stroke"
 import ImageHover from "@/src/components/common/ImageHover";
 import Navbar from "@/src/components/common/Navbar";
+import HorizontalGallery from "@/src/components/gallery/HorizontalGallery";
 
 export default function Home() {
   return (
     <>
     <div><Navbar/></div>
-    <div><Skiper19 />
+    <div>
+      <Skiper19 />
       <ImageHover/>
+      <HorizontalGallery/>
       <Footer/>
-</div>
+    </div>
     
     
       
