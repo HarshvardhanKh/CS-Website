4cdc55284ff640e84abb0f39f26932d8b2b78296
HarshvardhanKh <harshvardhan.khaitan1202@gmail.com>
2026-02-16 01:31:27 +0530

    Added Team Card

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import ImageHover from "@/src/components/common/ImageHover";
 import Navbar from "@/src/components/common/Navbar";
 import HorizontalGallery from "@/src/components/gallery/HorizontalGallery";
 import TargetCursor from "@/src/components/common/TargetCursor";
+import TeamCard from "@/src/components/common/TeamCard";
 
 export default function Home() {
   return (
     <>
 
+    
     <TargetCursor 
         spinDuration={2}
         hideDefaultCursor
@@ -19,6 +21,19 @@ export default function Home() {
     <div>
       <Skiper19 />
       <ImageHover/>
+      
+      <TeamCard
+        image="https://images.pexels.com/photos/34408249/pexels-photo-34408249.jpeg"
+        name="John Doe"
+        role="Lead Developer"
+        socials={{
+          linkedin: "https://linkedin.com/in/johndoe",
+          twitter: "https://twitter.com/johndoe",
+          github: "https://github.com/johndoe"
+        }}
+        className="w-[400px] h-[600px]"
+      />
+      
       <HorizontalGallery/>
       <Footer/>
     </div>
